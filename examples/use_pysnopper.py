c176a7d60951961939e5d2741c9f63a851317b5d
13717910964 <13717910964@163.com>
2019-06-15 21:21:26 +0800

    use pysnopper

---
+++ examples/use_pysnopper.py
@@ -0,0 +1,34 @@
+
+# https://github.com/cool-RR/pysnooper
+
+import pysnooper
+
+@pysnooper.snoop()
+def number_to_bits(number):
+    if number:
+        bits = []
+        while number:
+            number, remainder = divmod(number, 2)
+            bits.insert(0, remainder)
+        return bits
+    else:
+        return [0]
+
+number_to_bits(6)
+
+
+####
+import random
+
+def foo():
+    lst = []
+    for i in range(10):
+        lst.append(random.randrange(1, 1000))
+
+    with pysnooper.snoop():
+        lower = min(lst)
+        upper = max(lst)
+        mid = (lower + upper) / 2
+        print(lower, mid, upper)
+
+foo()
